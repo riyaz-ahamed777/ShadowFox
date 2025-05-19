63cecb60b449fbc4488a6d678d19b21f1b40fb56
Riyaz Ahamed N <riyazahamed33221@gmail.com>
2025-05-19 20:29:13 +0530

    Add files via upload

---
+++ Intermediate Level/Task 2/Bank Account Management System/src/main/java/Transaction.java
@@ -0,0 +1,30 @@
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return timestamp + " - " + type + ": $" + amount;
+    }
+}
